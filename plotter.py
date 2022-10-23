1104dbbb05fa10d0f2c94d51e28a685c8b9c0c74
axiomcura <erikishere3@gmail.com>
2022-10-23 17:09:26 -0600

    added scatter plot function

--- plotter.py
+++ plotter.py
@@ -53,6 +53,59 @@ def create_iris_boxplot(iris_df: pd.DataFrame, outname: str) -> None:
     ax.boxplot(data.values())
     ax.set_xticklabels(data.keys())
     ax.set_ylabel("cm")
+    ax.set_title("Iris boxplot of all species")
+
+    # saving plot
+    plt.savefig(outname)
+
+
+def petal_width_v_length_scatter(iris_df: pd.DataFrame, outname: str) -> None:
+    """_summary_
+
+    Parameters
+    ----------
+    iris_df : pd.DataFrame
+        _description_
+    outname : str
+        _description_
+
+    Return
+    ------
+    None
+        Generates an image file
+
+    Raises
+    ------
+    TypeError
+        raised if iris_df is not a data frame or out name is not a string type
+    ValueError
+        raised if expected column values do not match from extracted data
+    """
+
+    # type checking
+    if not isinstance(iris_df, pd.DataFrame):
+        raise TypeError("Iris data must be a pandas dataframe")
+    if not isinstance(outname, str):
+        raise TypeError(f"outname must be a string, not {type(outname)}")
+
+    # setting up subplots
+    fig, ax = plt.subplots()
+
+    # plotting data based on species
+    species = iris_df["iris_species"].unique().tolist()
+    for species_name in species:
+        iris_subset = iris_df[iris_df["iris_species"] == species_name]
+        plt.scatter(
+            iris_subset["sepal_length"],
+            iris_subset["sepal_width"],
+            label=species_name,
+        )
+
+    # labeling axis
+    ax.set_ylabel("sepal_width (cm)")
+    ax.set_xlabel("sepal_length (cm)")
+    ax.set_title("Sepal Length vs Sepal Width based on Species")
+    plt.legend()
 
     # saving plot
     plt.savefig(outname)
