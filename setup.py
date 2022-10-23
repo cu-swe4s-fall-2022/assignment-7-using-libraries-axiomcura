6a180aeab02f5769894969053a2c937dcb46182d
axiomcura <erikishere3@gmail.com>
2022-10-22 21:46:33 -0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Sets up DataProc package in your environment
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name="DataProc",
+    version="0.0.1",
+    author="Erik Serrano",
+    description="Processess tabular dataset",
+    packages=find_packages()
+)
