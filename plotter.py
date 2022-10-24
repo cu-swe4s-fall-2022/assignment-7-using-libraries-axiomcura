4315bf1f4884785e433daae2fadb291429655cd2
axiomcura <erikishere3@gmail.com>
2022-10-23 22:59:28 -0600

    added cli functionality to plotter.py

--- plotter.py
+++ plotter.py
@@ -1,7 +1,20 @@
-"""_summary_
+"""
+Script that plots iris data
 
 * create_iris_boxplot - Creates a box plot
+
+* petal_width_v_length_scatter - Creates a scatter plot comparing petal width
+and length
+
+* merged_boxplot_and_scatter - wrapper function that merges the two plot into
+one
+
 """
+import argparse
+import sys
+from pathlib import Path
+
+import data_processor as dp
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -124,7 +137,10 @@ def petal_width_v_length_scatter(
     plt.close()
     return ax
 
-def merged_boxplot_and_scatter(iris_df: pd.DataFrame, outname: str) -> plt.Axes:
+
+def merged_boxplot_and_scatter(
+    iris_df: pd.DataFrame, outname: str
+) -> plt.Axes:
     """plots both boxplot and scatter plot together and saves it.
 
     Parameters
@@ -146,14 +162,102 @@ def merged_boxplot_and_scatter(iris_df: pd.DataFrame, outname: str) -> plt.Axes:
         raise TypeError(f"outname must be a string, not {type(outname)}")
 
     fig, ax = plt.subplots(1, 2)
-    fig.set_size_inches(20,10)
-    create_iris_boxplot(iris_df, outname="box", axis=ax[0])
-    petal_width_v_length_scatter(iris_df, outname="scatter", axis=ax[1])
+    fig.set_size_inches(20, 10)
+    create_iris_boxplot(iris_df, outname=outname, axis=ax[0])
+    petal_width_v_length_scatter(iris_df, outname=outname, axis=ax[1])
 
     for i in range(len(ax)):
-        ax[i].spines['top'].set_visible(False)
-        ax[i].spines['right'].set_visible(False)
-        ax[i].spines['bottom'].set_visible(True)
-        ax[i].spines['left'].set_visible(True)
+        ax[i].spines["top"].set_visible(False)
+        ax[i].spines["right"].set_visible(False)
+        ax[i].spines["bottom"].set_visible(True)
+        ax[i].spines["left"].set_visible(True)
 
     plt.savefig(outname)
+
+
+def display_exception(e: BaseException) -> None:
+    """Displays error in terminal and appropriately exist the program
+
+    Parameters
+    ----------
+    e : Exception
+        Error exceptions
+
+    Returns
+    -------
+    None
+        prints out error message and exits with exit code 1
+    """
+    # extracting name and message from exception
+    try:
+        e_name = e.__class__.__name__
+        print(f"{e_name}: {e}")
+        sys.exit(1)
+    except BaseException:
+        raise TypeError("Exception class was not provided")
+
+
+def main() -> int:
+
+    # CLI arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--iris_data",
+        dest="iris_data",
+        required=True,
+        help="iris dataset",
+    )
+    parser.add_argument(
+        "-o",
+        "--outname",
+        dest="outname",
+        required=True,
+        help="outname of the generated files",
+    )
+    args = parser.parse_args()
+
+    # setting up output names
+    boxplot_outname = f"{args.outname}_boxplot"
+    scatter_outname = f"{args.outname}_scatterplot"
+    merge_outname = f"{args.outname}_merged_box_and_scatter"
+
+    # loading iris.data and convert into dataframe
+    try:
+        iris_df = dp.read_data_file(args.iris_data)
+    except FileNotFoundError as e:
+        display_exception(e)
+    except PermissionError as e:
+        display_exception(e)
+    except RuntimeError as e:
+        display_exception(e)
+
+    # generate plots
+    # -- creating box plot
+    try:
+        create_iris_boxplot(iris_df=iris_df, outname=boxplot_outname)
+    except TypeError as e:
+        display_exception(e)
+    except ValueError as e:
+        display_exception(e)
+
+    # -- create scatter plot
+    try:
+        petal_width_v_length_scatter(iris_df=iris_df, outname=scatter_outname)
+    except TypeError as e:
+        display_exception(e)
+    except ValueError as e:
+        display_exception(e)
+
+    # -- generate merged boxplot and scatter plot in one figure
+    try:
+        merged_boxplot_and_scatter(iris_df=iris_df, outname=scatter_outname)
+    except Exception as e:
+        display_exception(e)
+
+    print("Plotter complete!")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
