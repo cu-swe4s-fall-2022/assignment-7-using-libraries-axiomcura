02e00ecc735a6f6096762dcebdd5adcb4bfc7335
axiomcura <erikishere3@gmail.com>
2022-10-23 18:14:46 -0600

    added merge plot function

--- plotter.py
+++ plotter.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def create_iris_boxplot(iris_df: pd.DataFrame, outname: str) -> None:
+def create_iris_boxplot(
+    iris_df: pd.DataFrame, outname: str, axis=None
+) -> plt.Axes:
     """Creates a box plot of sepal width/length and petal width/length.
 
     Parameters
@@ -50,6 +52,9 @@ def create_iris_boxplot(iris_df: pd.DataFrame, outname: str) -> None:
 
     # creating subplot
     fig, ax = plt.subplots()
+    if axis is not None:
+        ax = axis
+
     ax.boxplot(data.values())
     ax.set_xticklabels(data.keys())
     ax.set_ylabel("cm")
@@ -57,11 +62,14 @@ def create_iris_boxplot(iris_df: pd.DataFrame, outname: str) -> None:
 
     # saving plot
     plt.savefig(outname)
+    plt.close()
 
-    return(ax)
+    return ax
 
 
-def petal_width_v_length_scatter(iris_df: pd.DataFrame, outname: str) -> plt.Axes:
+def petal_width_v_length_scatter(
+    iris_df: pd.DataFrame, outname: str, axis=None
+) -> plt.Axes:
     """Creates a scatter plot that compares all sepal widths across all species
     . Returns axis object that contains all the plot data.
 
@@ -92,10 +100,13 @@ def petal_width_v_length_scatter(iris_df: pd.DataFrame, outname: str) -> plt.Axe
 
     # plotting data based on species
     fig, ax = plt.subplots()
+    if axis is not None:
+        ax = axis
+
     species = iris_df["iris_species"].unique().tolist()
     for species_name in species:
         iris_subset = iris_df[iris_df["iris_species"] == species_name]
-        plt.scatter(
+        ax.scatter(
             iris_subset["sepal_length"],
             iris_subset["sepal_width"],
             label=species_name,
@@ -105,9 +116,44 @@ def petal_width_v_length_scatter(iris_df: pd.DataFrame, outname: str) -> plt.Axe
     ax.set_ylabel("sepal_width (cm)")
     ax.set_xlabel("sepal_length (cm)")
     ax.set_title("Sepal Length vs Sepal Width based on Species")
-    plt.legend()
+    ax.legend()
 
     # saving plot
     plt.savefig(outname)
 
-    return (ax)
+    plt.close()
+    return ax
+
+def merged_boxplot_and_scatter(iris_df: pd.DataFrame, outname: str) -> plt.Axes:
+    """plots both boxplot and scatter plot together and saves it.
+
+    Parameters
+    ----------
+    iris_df : pd.DataFrame
+        iris data frame
+    outname : str
+        outname of generated image
+
+    Returns
+    -------
+    None
+        Merged image with named by outname in current directory
+    """
+    # type checking
+    if not isinstance(iris_df, pd.DataFrame):
+        raise TypeError("Iris data must be a pandas dataframe")
+    if not isinstance(outname, str):
+        raise TypeError(f"outname must be a string, not {type(outname)}")
+
+    fig, ax = plt.subplots(1, 2)
+    fig.set_size_inches(20,10)
+    create_iris_boxplot(iris_df, outname="box", axis=ax[0])
+    petal_width_v_length_scatter(iris_df, outname="scatter", axis=ax[1])
+
+    for i in range(len(ax)):
+        ax[i].spines['top'].set_visible(False)
+        ax[i].spines['right'].set_visible(False)
+        ax[i].spines['bottom'].set_visible(True)
+        ax[i].spines['left'].set_visible(True)
+
+    plt.savefig(outname)
